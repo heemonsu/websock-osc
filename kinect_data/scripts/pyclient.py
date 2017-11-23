eb03bc0af726167183a1fb6c3104aae3521fccaa
heemonsu <d.cool.x.stranger@gmail.com>
2017-11-22 20:37:07 -0500

    update: publish skeleton data

--- kinect_data/scripts/pyclient.py
+++ kinect_data/scripts/pyclient.py
@@ -1,22 +1,66 @@
+#!/usr/bin/env python
+
 import websocket
 import thread
 import time
 
 import rospy
-import std_msgs
 
+from std_msgs.msg import String
+from kinect_data.msg import skeleton
 
-def on_message(ws, message):
+#skeleton = []
+#global pub
+#pub = rospy.Publisher('skeleton_data', skeleton, queue_size=10)
+#global r
+#r = skeleton()
+#global num_of_updates
+#num_of_updates = [0, 0, 0, 0] 
+
+def on_message(ws, message, num=[0,0,0,0], r=skeleton()):
 	words = message[1:-1].split(",")
-	skeleton = []
-	pub = rospy.Publisher('pyclient/skeleton_data', std_msgs.MultiArray, quee_size=1)
-	if words[0] == "Right_Shoulder" or "Right_Hand" or "Left Shoulder" or "Left Wrist":
-		words_timestamped = words.append(0)
-		skeleton.append(words_timestamped)
-	if len(skeleton) == 4:
-		# Write the correct publish data
-		pub.publish(words_timestamped)
-		rospy.loginfo(words_timestamped)
+	print(words[0])
+	pub = rospy.Publisher('skeleton_data', skeleton, queue_size=1)
+	if words[0][1:-1] == "/NI_mate_sync":
+		# print("NI mate sync")
+		num =  [0, 0, 0, 0]
+		r = skeleton()
+		pub = rospy.Publisher('skeleton_data', skeleton, queue_size=5)	
+	elif words[0][1:-1] == "Right_Shoulder":
+		r.joints[0].joint_name = words[0]
+		r.joints[0].x = words[1]
+		r.joints[0].y = words[2]
+		r.joints[0].z = words[3]
+		r.joints[0].stamp = rospy.get_time()
+		num[0] = 1
+	elif words[0][1:-1] == "Left_Shoulder":
+		r.joints[1].joint_name = words[0]
+		r.joints[1].x = words[1]
+		r.joints[1].y = words[2]
+		r.joints[1].z = words[3]
+		r.joints[1].stamp = rospy.get_time()
+		num[1] =  1
+	elif words[0][1:-1] == "Right_Hand":
+		r.joints[2].joint_name = words[0]
+		r.joints[2].x = words[1]
+		r.joints[2].y = words[2]
+		r.joints[2].z = words[3]
+		r.joints[2].stamp = rospy.get_time()
+		num[2] = 1
+	elif words[0][1:-1] == "Left_Hand":
+		r.joints[3].joint_name = words[0]
+		r.joints[3].x = words[1]
+		r.joints[3].y = words[2]
+		r.joints[3].z = words[3]
+		r.joints[3].stamp = rospy.get_time()
+		num[3] = 1
+		
+		if sum(num) == 4:
+			# Write the correct publish data
+			pub.publish(r)
+			rospy.loginfo(r)
+	print(num)
+
 
 def on_error(ws, error):
 	print(error)
@@ -32,8 +76,9 @@ def on_open(ws):
 if __name__ == '__main__':
 	try:
 		rospy.init_node('kinect_data', anonymous=True)
-		websocket.enableTruce(True)
-		ws = websocket.WebSocketApp("ws://130.215.206.245:3000", on_message=on_message, on_error=on_error, on_close=on_close)
+		
+		websocket.enableTrace(True)
+		ws = websocket.WebSocketApp("ws://130.215.206.204:3000", on_message=on_message, on_error=on_error, on_close=on_close)
 		ws.on_open = on_open
 		ws.run_forever()
 	except KeyboardInterrupt:
